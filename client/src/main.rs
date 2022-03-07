0d89991ff4ae3f31a33c5a94bd31ef5f8fa6b039
Chris Jeakle <cjjeakle@umich.edu>
2022-03-07 00:26:05 -0600

    Work a bit on concurrent access

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,57 +1,59 @@
 #![feature(map_first_last)]
 use std::collections::BTreeMap;
 use chrono::{DateTime, Utc};
+use chrono::Duration as chrono_Duration;
 use std::time::Duration;
 use std::sync::{Mutex};
 use std::thread;
 use actix_web::{get, web, App, HttpServer, Responder};
 
+
 mod config;
 
 struct PingData {
     data: BTreeMap<DateTime<Utc>, Duration>,
-    mutex: Mutex<()>
 }
 impl PingData {
     fn AddEntry(&mut self, when: DateTime<Utc>, how_long: Duration) {
-        let _guard = self.mutex.lock().unwrap();
-        let locked_data = &mut self.data;
-        if locked_data.len() >= config::MAX_ENTRIES_SAVED {
-            locked_data.pop_first();  // Drop the oldest entry
+        if self.data.len() >= config::MAX_ENTRIES_SAVED {
+            self.data.pop_first();  // Drop the oldest entry
         }
-        locked_data.insert(when, how_long);
+        self.data.insert(when, how_long);
     }
     fn ForEach(self, callback: fn(&DateTime<Utc>, &Duration)) {
-        let _guard = self.mutex.lock().unwrap();
-        let locked_data = &self.data;
-        for (when, how_long) in locked_data {
-            callback(when, how_long);
+        for (when, how_long) in self.data {
+            callback(&when, &how_long);
         }
     }
 }
-static mut PINGS: PingData = PingData{
-    data: BTreeMap::new(),
-    mutex: Mutex::new(())
-};
+static mut PINGS: Mutex<PingData> = Mutex::new(PingData{
+    data: BTreeMap::new()
+});
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    thread::spawn(ping);
+    thread::spawn(repeatedly_ping);
     return HttpServer::new(|| App::new().service(index))
         .bind(("127.0.0.1", config::WEB_PORT))?
         .run()
         .await
 }
 
-fn ping() {
-    loop {        
+fn repeatedly_ping() {
+    loop {
         let start_time: DateTime<Utc> = Utc::now();
-        let how_long = Duration::from_millis(10);  // TODO
-        // Kick off a thread to potentially wait to update the map.
-        thread::spawn(move || {
-            PINGS.AddEntry(start_time, how_long);
+        let how_long = Duration::from_millis(10);  // TODO: IMPLEMENT <=====================
+        // Kick off a worker thread to lock and update the map.
+        thread::spawn(|| {
+            let locked_data = PINGS.lock().unwrap();
+            locked_data.AddEntry(start_time, how_long);
         });
-        thread::sleep(Duration::from_secs(config::SEC_BETWEEN_PINGS));
+        // Wait for the ping interval to elapse.
+        let next_ping_time = start_time + chrono_Duration::seconds(config::SEC_BETWEEN_PINGS as i64);
+        let cur_time = Utc::now();
+        if cur_time < next_ping_time {
+            thread::sleep((next_ping_time - cur_time).to_std().unwrap());
+        }
     }
 }
 
