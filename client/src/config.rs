15079fee9eac171415b709a0922c4ae0431a15fc
Chris Jeakle <cjjeakle@umich.edu>
2022-05-03 19:34:49 -0500

    Fixed data retention period (was 1/60 of intended config)

--- client/src/config.rs
+++ client/src/config.rs
@@ -1,4 +1,4 @@
 pub const SEC_BETWEEN_PINGS: u64 = 5;
 pub const PING_TIMEOUT_MSEC: u64 = 1_000;
-pub const MAX_ENTRIES_SAVED: usize = 7 * 24 * (60 / SEC_BETWEEN_PINGS as usize); // 1 week
+pub const MAX_ENTRIES_SAVED: usize = 7 * 24 * 60 * (60 / SEC_BETWEEN_PINGS as usize); // 1 week
 pub const WEB_UI_PORT: u16 = 8180;
